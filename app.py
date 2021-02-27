44636a4df8ebf804d068103daa125ec5840e035a
Dustin Elery <delery7@yahoo.com>
2021-02-27 14:11:23 -0500

    app.py file is working as expected

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
+# Pull in packages
+
 import numpy as np
 import pandas as pd
 import datetime as dt
 import json
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
-
-
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
+
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
 Station = Base.classes.station
 Measurement= Base.classes.measurement
+
 # getting the latest date and one year back
 latest_date=dt.date(2017,8,23)
 delta=dt.timedelta(days=365)
@@ -44,11 +43,20 @@ one_year=latest_date-delta
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/startdate:/<start><br/>"
+        f"Available Routes:<br/><br/>"
+        f"This api will get you all of the precipation data by date going back one year from the latest date in the data set:<br/>"
+        f"/api/v1.0/precipitation<br/><br/>"
+        f"This api will get you a list of the stations in the data set:<br/>"
+        f"/api/v1.0/stations<br/><br/>"
+        f"This api will get you the temperature data for the last year from the most active station in the data set:<br/>"
+        f"/api/v1.0/tobs<br/><br/>"
+        f"This api will get you the min, avg, and max temperature data going back to the entered start date:<br/>"
+        f"Put in a date at the end of this string as Month-Day-Year.<br/> Example: http://127.0.0.1:5000//api/v1.0/startdate:/10-11-2016 <br/>"
+        f"(Data returns in Min, Avg, Max order)<br/>"
+        f"/api/v1.0/startdate:/<start><br/><br/>"
+        f"This api will get you the min, avg, and max temperature data from the entered start date to the end date:<br/>"
+        f"Put in a date after each date :/ as Month-Day-Year.<br/> Example: http://127.0.0.1:5000//api/v1.0/startdate:/10-11-2016/enddate:/12-10-2016<br/>"
+        f"(Data returns in Min, Avg, Max order)<br/>"
         f"/api/v1.0/startdate:/<start>/enddate:/<end>"
     )
 
@@ -61,7 +69,7 @@ def date_prcp():
     results=session.query(Measurement.date,Measurement.prcp).filter(Measurement.date>one_year).all()
     session.close()
 
-    # Convert list of tuples into normal list
+    # Convert list into normal list
     date_prcp = list(np.ravel(results))
 
     return jsonify(date_prcp)
@@ -74,7 +82,7 @@ def stations():
     # Query for stations
     results=session.query(Station.name).filter(Measurement.station==Station.station).group_by(Measurement.station).all()
     session.close()
-    # Convert list of tuples into normal list
+    # Convert list into normal list
     stations = list(np.ravel(results))
 
     return jsonify(stations)
@@ -89,7 +97,8 @@ def active():
         filter(Measurement.date>one_year).\
         filter(Measurement.station=="USC00519281").all()
     session.close()
-    # Convert list of tuples into normal list
+
+    # Convert list into normal list
     active = list(np.ravel(results))
 
     return jsonify(active)
@@ -98,36 +107,41 @@ def active():
 def startdate(start):
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    # start = dt.datetime.strptime(start, '%Y-%m-%d')
-    # end = dt.datetime.strptime(start, '%Y-%m-%d')   
-    # Query for start date
+    start = dt.datetime.strptime(start, '%m-%d-%Y')
+       
+    # Query for data from the user defined start date
+
     inter=[func.min(Measurement.tobs),func.avg(Measurement.tobs), func.max(Measurement.tobs)]
     results=session.query(*inter).\
         filter(Measurement.date>=start).all()
-    # print(results)
+
     session.close()
+
     # Convert list of tuples into normal list
-    # results2=json.dumps(results)
     startdate = list(np.ravel(results))
-    # startdate = np.ravel(results)
-    # print(startdate)
+
     return jsonify(startdate)
 
 
-# @app.route("/api/v1.0/startdate:/<start>/enddate:/<end>")
-# def active():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+@app.route("/api/v1.0/startdate:/<start>/enddate:/<end>")
+def startend(start,end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    start = dt.datetime.strptime(start, '%m-%d-%Y')
+    end = dt.datetime.strptime(end, '%m-%d-%Y')   
+    
+    # Query for data from the user defined start date
+    inter=[func.min(Measurement.tobs),func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    results=session.query(*inter).\
+        filter(Measurement.date>=start).filter(Measurement.date<=end).all()
 
-#     # Query all passengers
-#     results=session.query(Measurement.station, Measurement.date, Measurement.tobs).\
-#         filter(Measurement.date>one_year).\
-#         filter(Measurement.station=="USC00519281").all()
-#     session.close()
-#     # Convert list of tuples into normal list
-#     active = list(np.ravel(results))
+    session.close()
 
-#     return jsonify(active)
+    # Convert list of tuples into normal list
+
+    startend = list(np.ravel(results))
+    
+    return jsonify(startend)
 
 if __name__ == '__main__':
     app.run(debug=True)
